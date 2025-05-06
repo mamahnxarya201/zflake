be7a6fd64811cd7b56409c82a22544b2d206b923
Aryana Diaz <diazcakasana201@gmail.com>
2025-05-06 14:35:26 +0700

    add custom test runner into build system

--- build.zig
+++ build.zig
@@ -11,11 +11,17 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     const zflake_unit_tests = b.addTest(.{
         .root_source_file = b.path(b.pathJoin(&.{"src", "test.zig"})),
+        // .test_runner = b.path("test_runner.zig"), now they expect ?Step.Compile.TestRunner not just lazyPath..
         .target = target,
         .optimize = optimize
     });
 
+
     zflake_unit_tests.root_module.addImport("zflake", zflake);
+    zflake_unit_tests.test_runner = .{
+        .path = b.path("test_runner.zig"),
+        .mode = .simple
+    };
     const run_zflake_tests = b.addRunArtifact(zflake_unit_tests);
     test_step.dependOn(&run_zflake_tests.step);
 }
